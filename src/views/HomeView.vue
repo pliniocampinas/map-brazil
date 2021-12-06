0dd50a518b634d42cd984f37a7b10cd5e66655ae
Plinioc <plinio.lsc@gmail.com>
2021-12-05 21:11:51 -0300

    Import svg maps with vue inline library and select key values

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,65 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <div class="map__states">
+      <inline-svg 
+          :src="require('../assets/brazil-states.svg')"
+          @loaded="statesSvgLoaded($event)"
+          @error="svgLoadError($event)"
+          width="500" 
+          fill="black"
+          aria-label="Brazillian States Svg Map"
+      ></inline-svg>
+    </div>
+    <div class="map__municipalities">
+      <inline-svg 
+          :src="require('../assets/brazil-municipalities.svg')"
+          @loaded="municipalitiesSvgLoaded($event)"
+          @error="svgLoadError($event)"
+          fill="black"
+          aria-label="Brazillian Municipalities Svg Map"
+      ></inline-svg>
+    </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import { defineComponent, onMounted } from 'vue';
+import InlineSvg from 'vue-inline-svg';
 
 export default defineComponent({
   name: 'HomeView',
+
   components: {
-    HelloWorld,
+    InlineSvg
   },
+
+  setup() {
+    const statesSvgLoaded = (e: any) => {
+      console.log('statesSvgLoaded', e)
+      const stateList = [...document.querySelectorAll('.estado')].map(estado => estado.getAttribute('xlink:href'))
+      console.log('stateList ', stateList);
+    }
+    const svgLoadError = (e: any) => {
+      console.log('svgLoadError', e)
+    }
+
+    const municipalitiesSvgLoaded = (e: any) => {
+      console.log('municipalitiesSvgLoaded', e)
+      const municipalitiesList = [...document.querySelectorAll('.map__municipalities path')].map(muni =>muni.getAttribute('inkscape:label'))
+      console.log('municipalitiesList ', municipalitiesList);
+    }
+
+    onMounted(() => {
+      const stateList = [...document.querySelectorAll('.estado')].map(estado => estado.getAttribute('xlink:href'))
+      console.log('onMounted', stateList);
+    })
+
+    return {
+      statesSvgLoaded,
+      municipalitiesSvgLoaded,
+      svgLoadError,
+    }
+  }
 });
 </script>
