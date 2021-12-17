e541322831da81e15cb457271e514615fb7d4069
Plinioc <plinio.lsc@gmail.com>
2021-12-17 03:35:41 -0300

    Remove data files to gist repositories and fetch from it

--- src/utils/stateMapHelper.ts
+++ src/utils/stateMapHelper.ts
@@ -14,7 +14,8 @@ const stateSchemaToGraph = (stateCsv: StatesCsvSchema): StateGraphData => {
 }
 
 export const fetchData = async () => {
-  const response = await fetch(document.location.origin + '/pib-per-capita-estados-2019.csv')
+  const csvUrl = 'https://gist.githubusercontent.com/pliniocampinas/17270d89ff1e99a91892e810a7d662b7/raw/43fdd17f3bf2d3d31239181ba79d72facef726bf/pib-per-capita-estados-2019.csv'
+  const response = await fetch(csvUrl)
     .then(response => response.text())
     .then(v => d3.csvParse(v)) as StatesCsvSchema[]
 
