b7d0c853e70ef8ed861233378df4759e149ac2f0
Plinioc <plinio.lsc@gmail.com>
2022-04-24 21:57:02 -0300

    Enhance control buttons

--- src/components/TimelineControl.vue
+++ src/components/TimelineControl.vue
@@ -4,14 +4,21 @@
   >
     <div class="timeline-control__display">
       <div class="timeline-control__current">
-        {{ currentValue }}
+        <strong>
+          {{ currentValue }}
+        </strong>
       </div>
-      <button @click="handlePlay" :disabled="isPlaying">Play</button>
+      <button
+        class="time-line__play-button"
+        @click="handlePlay"
+        :class="isPlaying? 'pause': 'play'"
+        :disabled="isPlaying"
+      ></button>
     </div>
     <div class="timeline-control__ticks">
       <div
         class="timeline-control__tick"
-        :class="i <= (currentValue - firstValue + 1)? 'timeline-control__tick--filled': ''"
+        :class="resolveTickClass(i)"
         v-for="i in ticks"
         :key="i"
         :style="`min-width: ${100/ticks}%`"
@@ -58,9 +65,20 @@ export default defineComponent({
       emit('play')
     }
 
+    const resolveTickClass = (index: number) => {
+      if(index === (props.currentValue - props.firstValue + 1)) {
+        return 'timeline-control__tick--active'
+      }
+      if(index < (props.currentValue - props.firstValue + 1)) {
+        return 'timeline-control__tick--filled'
+      }
+      return ''
+    }
+
     return {
       handleTickClick,
       handlePlay,
+      resolveTickClass,
     }
   },
 });
@@ -74,12 +92,21 @@ export default defineComponent({
   grid-template-columns: 1fr;
 }
 
+.timeline-control__display {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
+  height: 28px;
+}
+
 .timeline-control__ticks {
   display: flex;
   height: 16px;
 }
 
 .timeline-control__tick {
+  cursor: pointer;
   background-color: whitesmoke;
   border: 1px solid black;
   box-sizing: border-box;
@@ -87,5 +114,26 @@ export default defineComponent({
 
 .timeline-control__tick--filled {
   background-color: lightseagreen;
+}
+
+.timeline-control__tick--active {
+  background-color: green;
+}
+
+.time-line__play-button {
+  box-sizing: border-box;
+  height: 20px;
+  width: 20px;
+  border-color: transparent transparent transparent #202020;
+  cursor: pointer;
+  /* play state */
+  border-style: solid;
+  border-width: 10px 0 10px 16px;
+}
+
+/* paused state */
+.time-line__play-button.pause {
+  border-style: double;
+  border-width: 0px 0 0px 16px;
 }
 </style>
