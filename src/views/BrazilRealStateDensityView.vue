21979df3f0bcaff027277d0e356d0c5a4ca27c36
Plinioc <plinio.lsc@gmail.com>
2023-01-31 17:46:06 -0300

    Implement densisty map with unotimized data

--- src/views/BrazilRealStateDensityView.vue
+++ src/views/BrazilRealStateDensityView.vue
@@ -3,7 +3,13 @@
     <MapBrowser :isLoading="isLoading">
       <template v-slot:map-svg>
         <div class="brazil-real-state-density__map-container">
-          <div class="placeholder" style="height: 300px;"></div>
+          <BrazilMunicipalitiesMap
+            class="brazil-real-state-density__map"
+            :selectedCityCode="selectedCityCode"
+            @city-click="cityClick"
+            @path-map-loaded="pathMapLoaded"
+          >
+          </BrazilMunicipalitiesMap>
         </div>
       </template>
 
@@ -21,14 +27,17 @@
 import { defineComponent, ref } from 'vue';
 import { fetchData as fetchAssets } from '@/services/GetAssetsService';
 import { sleep } from '@/utils/timeHelper';
+import BrazilMunicipalitiesMap from '@/components/BrazilMunicipalitiesMap.vue';
 import MapBrowser from '@/components/MapBrowser.vue';
 import Asset from '@/interfaces/Asset';
+import { Path } from 'd3-path';
 
 
 export default defineComponent({
   name: 'BrazilRealStateDensityView',
 
   components: {
+    BrazilMunicipalitiesMap,
     MapBrowser,
   },
 
@@ -36,8 +45,9 @@ export default defineComponent({
     const isLoading = ref(false)
     const assets = ref([] as Asset[])
     const pathElementsMap = ref<{ [code: string] : Element | null;}>({})
+    const selectedCityCode = ref('')
 
-    const statesSvgLoaded = async (pathMap: { [code: string] : Element | null; }) => {
+    const pathMapLoaded = async (pathMap: { [code: string] : Element | null; }) => {
       isLoading.value = true
       pathElementsMap.value = pathMap
       await sleep(400)
@@ -46,14 +56,55 @@ export default defineComponent({
       isLoading.value = false
     }
 
+    const addCircle = (gElement: Element, element: Element | null) => {
+      if(!element) {
+        return
+      }
+
+      const bbox = (element as SVGPathElement).getBBox()
+      const circleElement = document.createElementNS('http://www.w3.org/2000/svg', 'circle')
+      circleElement.setAttribute('r', '12')
+      circleElement.setAttribute('fill', 'var(--app-primary-color)')
+      circleElement.setAttribute('fill-opacity', '0.4')
+      circleElement.setAttribute('cx', bbox.x.toString())
+      circleElement.setAttribute('cy', bbox.y.toString())
+      gElement.appendChild(circleElement)
+    }
+
+    const normalizeCityName = (name: string) => name.normalize('NFD').replace(/\p{Diacritic}/gu, "")
+
     const colorizeMap = () => {
-      console.log('colorizeMap')
+      const gElement = document.querySelector('.brazil-real-state-density__map g')
+      if(!gElement) {
+        console.warn('SVG not rendered')
+        return
+      }
+      Object.entries(pathElementsMap.value).forEach(keyValuePair => {
+        const element  = keyValuePair[1]
+        const cityName = normalizeCityName(element?.getAttribute('description')?? '')
+        if(assets.value.find(asset => normalizeCityName(asset.city) === cityName)) {
+          addCircle(gElement, element)
+        }
+
+        element?.setAttribute('fill', 'var(--app-secondary-color)')
+      })
+    }
+
+    const cityClick = (code: string) => {
+      console.log('code', code)
+      if(selectedCityCode.value == code) {
+        selectedCityCode.value = ''
+        return
+      }
+      selectedCityCode.value = code;
     }
 
     return {
       assets,
       isLoading,
-      statesSvgLoaded,
+      selectedCityCode,
+      pathMapLoaded,
+      cityClick,
     }
   }
 });
@@ -63,4 +114,9 @@ export default defineComponent({
 .brazil-real-state-density__map-container {
   position: relative;
 }
+
+.brazil-real-state-density__map {
+  height: 500px;
+}
+
 </style>
